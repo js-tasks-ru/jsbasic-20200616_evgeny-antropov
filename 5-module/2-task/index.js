4311e707a7ef85cbd8a8f57113749529c9286988
squaredbusinessman <burdaonelove@gmail.com>
2020-07-19 12:07:43 +0300

    решения задач из раздела 5

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,12 @@
 function toggleText() {
-  // ваш код...
+  let button = document.querySelector('.toggle-text-button');
+  let text = document.getElementById('text');
+  console.log(text);
+  button.addEventListener('click', function () {
+    if (!text.hasAttribute('hidden')) {
+      text.setAttribute('hidden', true);
+    } else {
+      text.removeAttribute('hidden');
+    }
+  })
 }
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,60 @@
 function initCarousel() {
-  // ваш код...
+  let container = document.querySelector('.container');
+  let arrowLeft = document.querySelector('.carousel__arrow_left');
+  let arrowRight = document.querySelector('.carousel__arrow_right');
+  let slide = document.querySelector('.carousel__slide');
+  let carousel = document.querySelector('.carousel__inner');
+  let position = 0;
+  let slideWidth = slide.offsetWidth;
+  console.log("размер слайда:", slideWidth);
+  let maxPosition = -(3 * slideWidth);
+  console.log("maxPosition - ", maxPosition);
+
+  function leftShift() {
+    //сдвиг влево
+    position += slideWidth;
+    carousel.style.transform = `translate(-${position + 'px'}`;
+    return +position;
+    console.log("сдвиг влево, значение позиции - ", position);
+  }
+
+  function rightShift() {
+    //сдвиг вправо
+    position -= slideWidth;
+    carousel.style.transform = `translate(${position + 'px'})`;
+    console.log("сдвиг вправо значение позиции - ", position);
+  }
+
+  function slideMovement(button) {
+    if (button === arrowLeft) {
+      leftShift();
+    } else if (button === arrowRight) {
+      rightShift();
+    }
+  }
+
+  function checkPosition() {
+    //проверяем значение position и скрываем кнопки переключения слайдов
+    if (position === 0) {
+      arrowLeft.style.display = 'none';
+    } else if (position === maxPosition) {
+      arrowRight.style.display = 'none';
+    } else {
+      arrowRight.style.display = '';
+      arrowLeft.style.display = '';
+    }
+  }
+
+  checkPosition();
+
+  container.addEventListener('click', function(evt) {
+    let target = evt.target.closest('div.carousel__arrow');
+    console.log(target);
+
+    if (!target) return;
+
+    slideMovement(target);
+    checkPosition();
+  })
+
 }
